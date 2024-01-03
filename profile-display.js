5ecf373d5d61934e9ac3886a3bef39987791bcb8
octylacetate <95532283+octylacetate@users.noreply.github.com>
2024-01-03 17:33:38 +0500

    check

--- profile-display.js
+++ profile-display.js
@@ -28,18 +28,3 @@ profileName.style.visibility = "hidden";
 linkscontainer.style.backgroundColor = "#F6F1E9";
 linkscontainer.style.borderColor = "#FFD93D";
 }
-
-
-
-
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
-const container = document.getElementById('container');
-
-signUpButton.addEventListener('click', () => {
-	container.classList.add("right-panel-active");
-});
-
-signInButton.addEventListener('click', () => {
-	container.classList.remove("right-panel-active");
-});
